3fbb3238570dc938964dda1c9fd25bb7658dd2a5
tomoanis <tomoanis@yahoo-corp.jp>
2023-08-07 09:35:51 +0900

    変更した値を取得

--- popup.js
+++ popup.js
@@ -1,6 +1,18 @@
+// 保存されている値を取得して表示
+chrome.storage.local.get('userName', function(result) {
+    if (result.userName) {
+        document.getElementById('savedValue').textContent = 'Saved Value: ' + result.userName;
+    }
+});
+
 document.getElementById('saveButton').addEventListener('click', function() {
     const userName = document.getElementById('userName').value;
     chrome.storage.local.set({userName: userName}, function() {
-        console.log('Value is set to ' + userName);
+        // エラーチェック
+        if (chrome.runtime.lastError) {
+            console.error("保存中にエラーが発生しました:", chrome.runtime.lastError);
+        } else {
+            console.log('Value is set to ' + userName);
+        }
     });
 });
